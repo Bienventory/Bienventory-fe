f26ae497ff025f16c0925d65e99b486f70c68fe5
BenFrancka <benfrancka@gmail.com>
2021-10-13 21:41:28 -0700

    adds menu list functionality

--- src/components/app/Menu/MenuItemList.jsx
+++ src/components/app/Menu/MenuItemList.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
 
 export default function MenuList() {
+  const [menuList, setMenuList] = useState([]);
+  useEffect(() => {
+    const loadMenuList = async () => {
+      const menu = await fetch(
+        'https://bienventory.herokuapp.com/api/v1/menus'
+      );
+      const json = await menu.json();
+      setMenuList(json);
+    };
+    loadMenuList();
+  }, []);
+
   return (
-    <div>Menu List &gt; map links to menu detail / add menu item button</div>
+    <List>
+      {menuList.map(({ meal_name }, index) => (
+        <ListItem key={index}>
+          <ListItemText primary={meal_name} />
+        </ListItem>
+      ))}
+    </List>
   );
 }
