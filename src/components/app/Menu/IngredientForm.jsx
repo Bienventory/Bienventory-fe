574bb2d79c155debfc6cd128dec71bbcf59ced66
BenFrancka <benfrancka@gmail.com>
2021-10-13 21:13:31 -0700

    adds functionality to menuitemform and ingredientform, posts to backend

--- src/components/app/Menu/IngredientForm.jsx
+++ src/components/app/Menu/IngredientForm.jsx
@@ -1,32 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, TextField } from '@mui/material';
+import PropTypes from 'prop-types';
 
-const handleChange = () => {};
-
-export default function IngredientForm() {
+export default function IngredientForm({ handleIngredientArrayChange }) {
+  const [ingredient, setIngredient] = useState('');
+  const [quantity, setQuantity] = useState(0);
+  const handleIngredientChange = ({ target }) => {
+    setIngredient(target.value);
+  };
+  const handleQuantityChange = ({ target }) => {
+    setQuantity(target.value);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const ingredientObj = { name: ingredient, quantity };
+    handleIngredientArrayChange(ingredientObj);
+  };
   return (
     <form>
       <TextField
         id="ingredient-name"
         label="Ingredient Name"
         //change to controlled input variable ingredient_name
-        value="ingredient"
+        value={ingredient}
         margin="normal"
-        onChange={handleChange}
+        onChange={handleIngredientChange}
       />
 
       <TextField
         id="qty_needed"
         label="Quantity needed per single serving"
         //change to controlled input variable qty_needed
-        value="number"
+        value={quantity}
         margin="normal"
-        onChange={handleChange}
+        onChange={handleQuantityChange}
       />
 
-      <Button style={{ display: 'block' }} variant="contained">
+      <Button
+        onClick={handleSubmit}
+        style={{ display: 'block' }}
+        variant="contained"
+      >
         Submit
       </Button>
     </form>
   );
 }
+
+IngredientForm.propTypes = {
+  handleIngredientArrayChange: PropTypes.func.isRequired
+};
