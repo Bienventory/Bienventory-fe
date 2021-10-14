a55c8e608bb9f8c19b2e37393fc0af07f3e7ac54
Kubi Kubisiak <mckubisiak@gmail.com>
2021-10-14 10:29:38 -0700

    updated inventory unit drop down, finished menu item form function

--- src/components/app/UpdateInventoryForm.jsx
+++ src/components/app/UpdateInventoryForm.jsx
@@ -2,34 +2,28 @@ import { TextField } from '@mui/material';
 import React from 'react';
 import Papa from 'papaparse';
 import { addSalesData } from '../../services/fetchUtils';
-
-
-
-const handleChange =  (event) => {
-  event.preventDefault();
-  
-  const files = event.target.files;
-  if (files) {
-    Papa.parse(files[0], {
-      delimiter: '',
-      header: true,
-      complete(results) {
-        console.log('Finished:', results.data);
-
-        addSalesData([
-          results.data, 
-          phoneNumber
-        ]) ;
-
-      },
-    });
-  }
-};
-
-
-
+import { useUser } from '../../hooks/LoginProvider';
 
 export default function UpdateInventoryForm() {
+  const user = useUser();
+
+  const handleChange = (event) => {
+    event.preventDefault();
+
+    const files = event.target.files;
+    if (files) {
+      Papa.parse(files[0], {
+        delimiter: '',
+        header: true,
+        complete(results) {
+          console.log('Finished:', results.data);
+
+          addSalesData([results.data, user.phone_number]);
+        },
+      });
+    }
+  };
+
   return (
     <form>
       <TextField
@@ -41,7 +35,6 @@ export default function UpdateInventoryForm() {
         margin="normal"
         onChange={handleChange}
       />
-
     </form>
   );
 }
