9ee9eb6391c8b422eef8a6d9787e55d16636ffc2
Kubi Kubisiak <mckubisiak@gmail.com>
2021-10-14 11:31:18 -0700

    csv post function added to button instead of on upload

--- src/components/app/UpdateInventoryForm.jsx
+++ src/components/app/UpdateInventoryForm.jsx
@@ -1,40 +1,49 @@
-import { TextField } from '@mui/material';
-import React from 'react';
+import { Button, TextField } from '@mui/material';
+import React, { useState } from 'react';
 import Papa from 'papaparse';
 import { addSalesData } from '../../services/fetchUtils';
 import { useUser } from '../../hooks/LoginProvider';
+import { useHistory } from 'react-router-dom';
 
 export default function UpdateInventoryForm() {
+  const [salesData, setSalesData] = useState([]);
   const user = useUser();
+  const history = useHistory();
 
   const handleChange = (event) => {
     event.preventDefault();
-
     const files = event.target.files;
     if (files) {
       Papa.parse(files[0], {
         delimiter: '',
+        dynamicTyping: true,
         header: true,
         complete(results) {
           console.log('Finished:', results.data);
-
-          addSalesData([results.data, user.phone_number]);
+          setSalesData(results.data, user.phone_number);
         },
       });
     }
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    await addSalesData(salesData);
+    history.push('/');
   };
 
   return (
     <form>
       <TextField
         id="sales-data"
-        label="Sales Data"
+        helperText="Sales Data"
         type="file"
         accept=".csv"
         value=""
         margin="normal"
         onChange={handleChange}
       />
+      <Button onClick={handleSubmit}> Submit</Button>
     </form>
   );
 }
