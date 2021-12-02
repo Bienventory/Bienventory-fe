b5d2b51ae1a2b507ffb8cc7cea1eaf7d225bd3e0
Kubi Kubisiak <mckubisiak@gmail.com>
2021-12-02 10:46:47 -0800

    added missing description to landing page

--- src/components/app/Login.jsx
+++ src/components/app/Login.jsx
@@ -12,7 +12,13 @@ export default function Login() {
   return (
     <div className="Login">
       <h2>Bienventory</h2>
-      <p>This will tell you about Bienventory!</p>
+      <p>
+        Bienventory was designed with restaurants in mind. Start by creating an
+        inventory with the scope of ingredients and meals.You are then able to
+        upload CSV files to update inventory and return a text notification.
+        This allows you to get accurate, up to date information about your
+        restaurant inventory.
+      </p>
       <GoogleLogin
         className="button"
         clientId={process.env.REACT_APP_CLIENT_ID}
